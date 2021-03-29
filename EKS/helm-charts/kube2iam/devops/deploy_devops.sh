237eec37ed2977f3b22486051f2ccf5763fc0ef2
dnewman <david_newman@stchome.com>
2021-03-29 14:46:22 -0700

    change

--- EKS/helm-charts/kube2iam/devops/deploy_devops.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-
-helm install stable/kube2iam \
-	--name kube2iam --namespace kube2iam -f values.yaml
--- EKS/helm-charts/nginx-ingress/stc-002/nginx-ing_Install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-helm install \
-	--name nginx-ingress \
-	--namespace nginx-ingress \
-	-f values.yaml \
-	stable/nginx-ingress
-
